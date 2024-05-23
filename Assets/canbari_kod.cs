5f9ce47e9ee959bd95c2cf3182809e11abffc9d7
Yigit G <yigitxgungormus@gmail.com>
2024-05-23 18:33:47 +0300

    Can Barı
    Canbarı kod ve tasarım

---
+++ Assets/canbari_kod.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cod : MonoBehaviour
+{
+    private Slider CanBari;
+    private int can = 100;
+
+
+    private void Awake()
+    {
+        CanBari = GameObject.Find("Slider").GetComponent<Slider>();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanBari.maxValue = 100;
+        CanBari.minValue = 0;
+        CanBari.value = can;
+        CanBari.wholeNumbers = true;// virgüllü deðerler almamasý için
+        CanBari.fillRect.GetComponent<Image>().color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("20 birim can azaldý");
+            can -= 20;
+            CanBari.value = can;
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("15 birim can arttý");
+            can += 15;
+            CanBari.value = can;
+        }
+    }
+}
