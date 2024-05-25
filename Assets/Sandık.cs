083137c609c799fad5b2d2efe5e20d7290d9c956
Yigit G <yigitxgungormus@gmail.com>
2024-05-25 16:13:00 +0300

    sandık anim
    sandık açma kapa animasyonları

--- Assets/Sandık.cs
+++ Assets/Sandık.cs
@@ -3,31 +3,54 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Sandık : MonoBehaviour
+public class Sandik : MonoBehaviour
 {
-    //public Animator anim;
-  public string diyalog;
-  public TextMeshProUGUI textI;
-   private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.CompareTag("Player")) // gelen obje tagı player ise
-        {
-            Debug.Log("Sandık Açılsın");
-            textI.text = diyalog;
-           // anim.SetBool("acikMi",true);
-        }
+    public Animator anim;
+    public string Diyalog;
+    public TextMeshProUGUI textI;
+    private bool oyuncuYakinda = false;
 
+    private void Update()
+    {
+        if (oyuncuYakinda && Input.GetKeyDown(KeyCode.F))
+        {
+            SandikAcKapa();
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) // gelen obje tagı player ise
+        {
+            oyuncuYakinda = true;
+            Debug.Log("Oyuncu sandık alanına girdi");
+            textI.text = Diyalog;
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) // gelen obje tagı player ise
         {
-            //anim.SetBool("acikMi",true);
-            Debug.Log("Sandık Kapatıldı");
+            oyuncuYakinda = false;
+            Debug.Log("Oyuncu sandık alanından çıktı");
             textI.text = " ";
-
+            anim.SetBool("acikMi", false);
         }
-
     }
-    
+
+    private void SandikAcKapa()
+    {
+        bool acikMi = anim.GetBool("acikMi");
+        anim.SetBool("acikMi", !acikMi);
+
+        if (!acikMi)
+        {
+            Debug.Log("Sandık açıldı");
+        }
+        else
+        {
+            Debug.Log("Sandık kapatıldı");
+        }
+    }
 }
