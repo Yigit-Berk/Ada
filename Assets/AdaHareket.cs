775f85918ba2841ebd51990c51a1c75c1ad82196
Yigit-Berk <yigithizarci@hotmail.com>
2024-05-25 15:22:04 +0300

    Gemi Diyalog işlemleri

--- Assets/AdaHareket.cs
+++ Assets/AdaHareket.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class AdaHareket : MonoBehaviour
 {
-    [SerializeField] GameObject gemi;
+    [SerializeField] GameObject gemiyeCarpan;
+    [SerializeField] GameObject diyalog;
+    
     // Start is called before the first frame update
     void Start()
     {
-        gemi = this.gameObject;
+        gemiyeCarpan = this.gameObject;
     }
     
     // Update is called once per frame
     void Update()
     {
         //Ada Gemiye doðru yaklaþýr
-        gemi.transform.Translate(new Vector3(-1 * Time.deltaTime, 0, 0)); // Time.deltaTime kullanarak her frame'de -1 birim hareket et
+        gemiyeCarpan.transform.Translate(new Vector3(-1 * Time.deltaTime, 0, 0)); // Time.deltaTime kullanarak her frame'de -1 birim hareket et
     }
 
     /*kinematic rigidbody'ye sahip olan Gemi ("Zemin katmaný") Tagli nesneye çarpma anýnda yapýlacaklar*/
@@ -24,7 +26,14 @@ public class AdaHareket : MonoBehaviour
         if (collision.gameObject.CompareTag("Gemi"))
         {
             Debug.Log("ADA GÖrüldü");
-            Destroy(gameObject);
+            Destroy(this.gameObject);
+
+            // Canvas'ý görünmez yap
+            if (diyalog != null)
+            {
+                diyalog.SetActive(true);
+            }
+
         }
     }
 
