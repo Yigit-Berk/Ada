0d1101cfb6a1af1edc14d60031088593ea914549
Yigit-Berk <yigithizarci@hotmail.com>
2024-05-29 14:41:21 +0300

    Tuzak ve kodları

---
+++ Assets/Scriptler/Karakter_Yasam.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Sonradan Eklenilen Kütüphane
+using UnityEngine.SceneManagement;
+
+//---------------------
+
+
+public class Karakter_Yasam : MonoBehaviour
+{
+    Animator animasyonDurumu;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        animasyonDurumu = this.GetComponent<Animator>();
+
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tuzak"))
+        {
+            karakterOlme();
+        }
+    }
+
+    private void karakterOlme()
+    {
+        animasyonDurumu.SetTrigger("Ölme");
+        rb.bodyType = RigidbodyType2D.Static;
+        YenidenBaslat();
+    }
+
+    private void YenidenBaslat()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //VEYA
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
